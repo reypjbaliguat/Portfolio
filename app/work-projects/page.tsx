0ca1eb2ff3649165c3ae1bb0b53cdf127bf053b9
reypjbaliguat <reypjbaliguat@gmail.com>
2024-01-11 01:20:57 +0800

    added id card store

--- app/work-projects/page.tsx
+++ app/work-projects/page.tsx
@@ -4,13 +4,14 @@ import React from "react";
 
 import Container from "@/app/components/Container";
 import WorkExperienceRow from "@/app/components/WorkProjectRow";
-import { BookingAppData, GGTAppData } from "@/data/WorkExperience";
+import { BookingAppData, GGTAppData, IdCardData } from "@/data/WorkExperience";
 
 export default function page() {
-    return (
-        <Container>
-            <WorkExperienceRow {...BookingAppData} />
-            <WorkExperienceRow {...GGTAppData} />
-        </Container>
-    );
+  return (
+    <Container>
+      <WorkExperienceRow {...BookingAppData} />
+      <WorkExperienceRow {...GGTAppData} />
+      <WorkExperienceRow {...IdCardData} />
+    </Container>
+  );
 }
