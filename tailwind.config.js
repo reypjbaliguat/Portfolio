11e893fcf4866a2251469034650d9695d809f9aa
reypjbaliguat <reypjbaliguat@gmail.com>
2024-03-07 17:46:50 +0800

    added primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
 
-        // Or if using `src` directory:
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        colors: {
-            blue: "rgb(14 165 233)",
-            white: "#ffffff",
-        },
-        extend: {},
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      blue: "rgb(14 165 233)",
+      white: "#ffffff",
     },
-    plugins: [],
+    extend: {
+      colors: {
+        primary: {
+          600: "#007EE6",
+        },
+        supporting: {
+          1: "#EDF3FF",
+        },
+      },
+    },
+  },
+  plugins: [],
 };
