86827534d8891e36ba71604f486bff6aac7e66c2
reypjbaliguat <reypjbaliguat@gmail.com>
2025-02-22 18:10:24 +0800

    feat(template): added new personal project

--- app/personal-projects/page.tsx
+++ app/personal-projects/page.tsx
@@ -10,6 +10,62 @@ export default function page() {
     <Container>
       <HalfContainers>
         <ColumnWithImage
+          source={"/personal-projects/template.PNG"}
+          alt="template-picture"
+        />
+      </HalfContainers>
+      <HalfContainers>
+        <BouncingButton buttonText={"Typing Game"} />
+
+        <DivWith4MarginTop>
+          <h1 className="font-semibold text-2xl">Built using:</h1>
+          <h1 className="font-bold text-2xl text-blue">
+            React JS, MUI, Mongo DB, GraphQL, Prisma and Node
+          </h1>
+        </DivWith4MarginTop>
+
+        <DivWith4MarginTop>
+          <h1 className="font-semibold text-2xl">Description:</h1>
+          <h1 className="font-semibold italic text-1xl">
+            ReplySaver is a simple yet powerful web tool designed to store and
+            organize email replies for quick access. With an intuitive
+            interface, you can save commonly used responses and retrieve them
+            instantly when needed. Each reply comes with a convenient copy
+            button, allowing you to paste responses effortlessly into your
+            emails with just one click. Key Features:
+          </h1>
+          <h1 className="font-semibold text-2xl">Key Features:</h1>
+          <div className="flex flex-col gap-y-2 mt-2">
+            <span>
+              ✅ Save & Organize – Store frequently used email replies in one
+              place.
+            </span>
+            <span>
+              ✅ One-Click Copy – Quickly copy responses with a single button.
+            </span>
+            <span>✅ Easy Access – Retrieve and reuse replies anytime.</span>
+            <span>
+              ✅ Streamlined Workflow – Save time by eliminating repetitive
+              typing.
+            </span>
+          </div>
+        </DivWith4MarginTop>
+        <DivWith4MarginTop>
+          <h1 className="font-semibold text-2xl">
+            Link:{" "}
+            <a
+              href="https://template-client-psi.vercel.app/"
+              target="_blank"
+              className="text-blue text-xl"
+              rel="noreferrer"
+            >
+              Click here
+            </a>
+          </h1>
+        </DivWith4MarginTop>
+      </HalfContainers>
+      <HalfContainers>
+        <ColumnWithImage
           source={"/personal-projects/Typing-game.PNG"}
           alt="typing-game-picture"
         />
@@ -54,15 +110,15 @@ export default function page() {
           </h1>
         </DivWith4MarginTop>
         <DivWith4MarginTop>
-          <h1 className="font-semibold text-2xl">Link:</h1>
-          <h1 className="font-bold text-2xl text-blue">
+          <h1 className="font-semibold text-2xl">
+            Link:{" "}
             <a
-              href="https://typing-game-v2.vercel.app/"
+              href="https://template-client-psi.vercel.app/"
               target="_blank"
-              className="text-blue"
+              className="text-blue text-xl"
               rel="noreferrer"
             >
-              https://typing-game-v2.vercel.app/
+              Click here
             </a>
           </h1>
         </DivWith4MarginTop>
