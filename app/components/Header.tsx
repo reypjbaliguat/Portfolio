a10b893a8150af4951c0b6246fd79fa45733c39f
reypjbaliguat <reypjbaliguat@gmail.com>
2025-03-24 14:16:22 +0800

    feat(nav): add animation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,21 +1,36 @@
-import React from "react";
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
 import LinkItem from "./LinkItem";
 
 export default function Header() {
-
+  const headerRef = useRef<HTMLDivElement>(null);
+  useGSAP(
+    () => {
+      gsap.to(".link-item", {
+        stagger: {
+          amount: 1,
+          from: "random",
+        },
+        opacity: 1,
+        duration: 0.8,
+        ease: "power2.inOut",
+      });
+    },
+    { scope: headerRef }
+  );
   return (
     <header className="flex justify-center shadow fixed top-0 left-0 right-0 bg-white z-50">
       <nav className="sm:w-9/12 w-10/12">
-        <div className="flex justify-center items-center py-4 w-full gap-x-4">
-          <LinkItem  route={"/"} text={"Home"} />
-          <LinkItem
-            route={"/personal-projects"}
-            text={"Personal Projects"}
-          />
-          <LinkItem
-            route={"/work-projects"}
-            text={"Work Projects"}
-          />
+        <div
+          className="flex justify-center items-center py-4 w-full gap-x-4"
+          ref={headerRef}
+        >
+          <LinkItem route={"/"} text={"Home"} />
+          <LinkItem route={"/personal-projects"} text={"Personal Projects"} />
+          <LinkItem route={"/work-projects"} text={"Work Projects"} />
         </div>
       </nav>
     </header>
--- app/components/LinkItem.tsx
+++ app/components/LinkItem.tsx
@@ -1,25 +1,63 @@
 "use client";
+import gsap from "gsap";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useRef } from "react";
 
 interface LinkItemProps {
   route: string;
   text: string;
 }
 
-export default function LinkItem({
-  route,
-  text,
-}: LinkItemProps) {
+export default function LinkItem({ route, text }: LinkItemProps) {
   const pathname = usePathname();
+  const linkRef = useRef<HTMLAnchorElement>(null);
+
+  const activeLinkStyles = {
+    boxShadow: "inset 200px 0 0 0 rgb(14 165 233)",
+    color: "#fff",
+  };
+
+  const inactiveLinkStyles = {
+    boxShadow: "inset 0 0 0 0 rgb(14 165 233)",
+    color: "black",
+  };
+
+  const activeLinkGsap = {
+    boxShadow: "inset 200px 0 0 0 rgb(14 165 233)",
+    color: "#fff",
+    duration: 0.3,
+    ease: "power2.out",
+  };
+
+  const inactiveLinkGsap = {
+    boxShadow: "inset 0 0 0 0 rgb(14 165 233)",
+    color: "black",
+    duration: 0.3,
+    ease: "power2.in",
+  };
+
+  const handleMouseEnter = () => {
+    gsap.to(linkRef.current, activeLinkGsap);
+  };
+
+  const handleMouseLeave = () => {
+    gsap.to(
+      linkRef.current,
+      pathname === route ? activeLinkGsap : inactiveLinkGsap
+    );
+  };
+
   return (
-      <Link
-        href={route}
-        className={`hover:text-blue hover:underline transition duration-300 ${
-          pathname === route && "text-blue underline"
-        }`}
-      >
-        <span className="text-1xl font-bold">{text}</span>
-      </Link>
+    <Link
+      ref={linkRef}
+      href={route}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={pathname === route ? activeLinkStyles : inactiveLinkStyles}
+      className={"px-4 py-2 rounded-md link-item opacity-0"}
+    >
+      <span className="text-1xl font-bold">{text}</span>
+    </Link>
   );
 }
