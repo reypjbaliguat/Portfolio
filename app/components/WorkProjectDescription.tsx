30945cd2e3060ce771cf893da8a52ef2ba859989
reypjbaliguat <reypjbaliguat@gmail.com>
2025-04-03 15:12:19 +0800

    feat(workProjects): add animation

--- app/components/WorkProjectDescription.tsx
+++ app/components/WorkProjectDescription.tsx
@@ -1,49 +1,63 @@
-import React from "react";
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import BouncingButton from "./BouncingButton";
 import DivWith4MarginTop from "./DivWith4MarginTop";
 import HalfContainer from "./HalfContainer";
 
 interface WorkProjectRowProps {
-    appName: string;
-    technologyUsed: string;
-    description: string;
-    paymentLink: string;
-    paymentName: string;
+  appName: string;
+  technologyUsed: string;
+  description: string;
+  paymentLink: string;
+  paymentName: string;
 }
 
 export default function WorkProjectDescription({
-    appName,
-    technologyUsed,
-    description,
-    paymentLink,
-    paymentName,
+  appName,
+  technologyUsed,
+  description,
+  paymentLink,
+  paymentName,
 }: WorkProjectRowProps) {
-    return (
-        <HalfContainer>
-            <BouncingButton buttonText={appName} />
-            <DivWith4MarginTop>
-                <h1 className="font-semibold text-2xl">Built using:</h1>
-                <h1 className="font-bold text-2xl text-blue">
-                    {technologyUsed}
-                </h1>
-            </DivWith4MarginTop>
-            <DivWith4MarginTop>
-                <h1 className="font-semibold text-2xl">Description:</h1>
-                <h1 className="font-semibold italic text-1xl">
-                    {description}
-                    {paymentLink && paymentName && (
-                        <a
-                            href={paymentLink}
-                            className="text-blue"
-                            target={"_blank"}
-                            rel="noreferrer"
-                        >
-                            {paymentName}
-                        </a>
-                    )}
-                    .
-                </h1>
-            </DivWith4MarginTop>
-        </HalfContainer>
-    );
+  useGSAP(() => {
+    gsap.to(".cwiElement", {
+      stagger: 0.2,
+      opacity: 1,
+      ease: "power2.inOut",
+    });
+  });
+  return (
+    <HalfContainer>
+      <BouncingButton buttonText={appName} className="cwiElement opacity-0" />
+      <DivWith4MarginTop>
+        <h1 className="font-semibold text-2xl cwiElement opacity-0">
+          Built using:
+        </h1>
+        <h1 className="font-bold text-2xl text-blue cwiElement opacity-0">
+          {technologyUsed}
+        </h1>
+      </DivWith4MarginTop>
+      <DivWith4MarginTop>
+        <h1 className="font-semibold text-2xl cwiElement opacity-0">
+          Description:
+        </h1>
+        <h1 className="font-semibold italic text-1xl cwiElement opacity-0">
+          {description}
+          {paymentLink && paymentName && (
+            <a
+              href={paymentLink}
+              className="text-blue"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              {paymentName}
+            </a>
+          )}
+          .
+        </h1>
+      </DivWith4MarginTop>
+    </HalfContainer>
+  );
 }
